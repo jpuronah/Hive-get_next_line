852b9e1290c5999ec5149326897ff243f84c6c21
jpuronah <jpuronah@student.hive.fi>
2022-02-01 12:13:30 +0200

    First commit to GitHub

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpuronah <jpuronah@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 16:21:30 by jpuronah          #+#    #+#             */
+/*   Updated: 2022/01/18 15:20:09 by jpuronah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+
+# define BUFF_SIZE 42
+# define MAX_FD 4097
+
+int	get_next_line(const int fd, char **line);
+
+#endif
